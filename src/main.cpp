d9b823adc5828681eb05e59ae8d676f0dc854d29
rodrigo <rodrigo@gato>
2015-04-20 21:11:05 -0300

    Initial files

---
+++ src/main.cpp
@@ -0,0 +1,18 @@
+/**
+ * Author: rodrigo
+ * 2015
+ */
+#include <stdlib.h>
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int x = 0;
+	int j = 10;
+
+	cout << "Hello world\n";
+
+	return EXIT_SUCCESS;
+}
