c70d195d0ba0c90ce0fcdeb8c85a8c7504bcd194
rodrigo <rodrigo@gato>
2015-10-27 11:20:34 -0300

    Fixed bugs in generation script

--- regenerate.sh
+++ regenerate.sh
@@ -2,13 +2,18 @@
 
 type=$1
 
+# Remove old build folder
 if [ -d "./build" ]; then
+	echo "Removing build folder"
 	rm -rf ./build
-	mkdir ./build/
 fi
 
-cd ./build/
+# Create build folder
+echo "Generating new build folder"
+mkdir ./build/
 
+# Generate with cmake
+cd ./build/
 if [ "$type" == "-r" ] ; then
 	echo "Generating project for release"
 	cmake -DCMAKE_BUILD_TYPE=Release ../src/
@@ -16,6 +21,18 @@ else
 	echo "Generating project for debug"
 	cmake -DCMAKE_BUILD_TYPE=Debug ../src/
 fi
-
 cd ..
+
+# Create folders if needed
+if [ ! -d "./input" ]; then
+	mkdir ./input/
+fi
+if [ ! -d "./output" ]; then
+	mkdir ./output/
+fi
+if [ ! -d "./cache" ]; then
+	mkdir ./cache/
+fi
+
+# Make 
 ./make.sh
