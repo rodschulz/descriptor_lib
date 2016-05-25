7176db3cc2037a980ac38f448e79010aa81f3ae3
rodrigo <rodrigo@gato>
2016-05-25 12:06:38 -0400

    Removed files that will be part of the "app" side

--- Test.cpp
+++
@@ -1,2 +0,0 @@
-#define BOOST_TEST_MODULE TestMain
-#include <boost/test/included/unit_test.hpp>
--- show.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-alias pcv='pcl_viewer'
-
-cd ./output/
-pcv pointPosition.pcd -use_point_picking &
-pcv cloud.pcd band* -use_point_picking  &
