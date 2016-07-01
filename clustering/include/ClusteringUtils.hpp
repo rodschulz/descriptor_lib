e811c15264211676b2298a1a3fa55ab75fcc8f40
rodrigo <rodrigo@gato>
2016-07-01 16:22:58 -0400

    Added utilities to label cloud using SVM

--- clustering/include/ClusteringUtils.hpp
+++ clustering/include/ClusteringUtils.hpp
@@ -5,10 +5,30 @@
 #pragma once
 
 #include <opencv2/core/core.hpp>
+#include <opencv2/ml/ml.hpp>
+#include <boost/algorithm/string.hpp>
+#include <boost/lexical_cast.hpp>
 #include "Metric.hpp"
+
+// SVM's shared pointer
+typedef boost::shared_ptr<CvSVM> CvSVMPtr;
 
 class ClusteringUtils
 {
+private:
+	// Generates a permutation of the given matrix
+	static inline void generatePermutation(const cv::Mat &matrix_, const int permutationSize_, const int permutationNumber_, cv::Mat &permutation_)
+	{
+		int begin = permutationNumber_ * permutationSize_;
+		int end = matrix_.cols;
+		matrix_.colRange(begin, end).copyTo(permutation_.colRange(0, end - begin));
+
+		begin = 0;
+		end = permutationNumber_ * permutationSize_;
+		if (end - begin > 0)
+			matrix_.colRange(begin, end).copyTo(permutation_.colRange(permutation_.cols - (end - begin), permutation_.cols));
+	}
+
 public:
 	// Labels the given data using the given centers
 	static inline void labelData(const cv::Mat &items_, const cv::Mat &centers_, const MetricPtr &metric_, cv::Mat &labels_)
@@ -32,8 +52,52 @@ public:
 	}
 
 	// Labels the given data using the given SVN as a classifier indicating the label
-	static inline void labelData(const cv::Mat &items_, const CvSVM &classifier_, cv::Mat &labels_)
+	static inline void labelData(const cv::Mat &items_, const CvSVMPtr &classifier_, cv::Mat &labels_)
 	{
-		svm->predict(items_, labels_);
+		classifier_->predict(items_, labels_);
+	}
+
+	// Prepares the clasificator for the labeling process
+	static CvSVMPtr prepareClasificator(const cv::Mat &centers_, const std::map<std::string, std::string> &centersParams_)
+	{
+		std::vector<std::string> params;
+		std::string metricParams = centersParams_.at("metric");
+		boost::trim_if(metricParams, boost::is_any_of("[]"));
+		boost::split(params, metricParams, boost::is_any_of("[,]"), boost::token_compress_on);
+
+		int permutationSize = centers_.cols;
+		if (boost::iequals(params[0], metricType[METRIC_CLOSEST_PERMUTATION]) || boost::iequals(params[0], metricType[METRIC_CLOSEST_PERMUTATION_WITH_CONFIDENCE]))
+			permutationSize = boost::lexical_cast<int>(params[1]);
+
+		int permutationNumber = centers_.cols / permutationSize;
+
+		// Prepare the training data
+		cv::Mat trainingData = cv::Mat::zeros(centers_.rows * permutationNumber, centers_.cols, CV_32FC1);
+		cv::Mat labels = cv::Mat::zeros(centers_.rows * permutationNumber, 1, CV_32FC1);
+
+		for (int i = 0; i < centers_.rows; i++)
+		{
+			cv::Mat currentCentroid = centers_.row(i);
+			for (int j = 0; j < permutationNumber; j++)
+			{
+				int index = i * permutationNumber + j;
+
+				cv::Mat permutation = trainingData.row(index);
+				generatePermutation(currentCentroid, permutationSize, j, permutation);
+				labels.at<float>(index, 0) = (float) (i + 1);
+			}
+		}
+
+		// Set SVM parameters
+		CvSVMParams svmParams;
+		svmParams.svm_type = CvSVM::C_SVC;
+		svmParams.kernel_type = CvSVM::LINEAR;
+		svmParams.term_crit = cvTermCriteria(CV_TERMCRIT_ITER + CV_TERMCRIT_EPS, 100, 1e-6);
+
+		// Train the SVM
+		CvSVMPtr svm = CvSVMPtr(new CvSVM());
+		svm->train(trainingData, labels, cv::Mat(), cv::Mat(), svmParams);
+
+		return svm;
 	}
 };
