7d90d1513e759de63a52d3413564207cfdafb06d
rodrigo <rodrigo@gato>
2016-07-25 10:00:47 -0400

    Moved point factory definitions to header file

--- factories/include/PointFactory.hpp
+++ factories/include/PointFactory.hpp
@@ -12,28 +12,116 @@ class PointFactory
 {
 public:
 	// Creates a point with XYZ coordinates
-	static pcl::PointXYZ createPointXYZ(const float _x, const float _y, const float _z);
+	static inline pcl::PointXYZ createPointXYZ(const float x_, const float y_, const float z_)
+	{
+		pcl::PointXYZ p;
+		p.x = x_;
+		p.y = y_;
+		p.z = z_;
+		return p;
+	}
 
 	// Creates a point with XYZ coordinates
-	static pcl::PointXYZ createPointXYZ(const Eigen::Vector3f &data_);
+	static inline pcl::PointXYZ createPointXYZ(const Eigen::Vector3f &data_)
+	{
+		pcl::PointXYZ p;
+		p.x = data_.x();
+		p.y = data_.y();
+		p.z = data_.z();
+		return p;
+	}
+
+	// Creates a point with XYZ coordinates and a label
+	static inline pcl::PointXYZL createPointXYZL(const float x_, const float y_, const float z_, const int label_)
+	{
+		pcl::PointXYZL p;
+		p.x = x_;
+		p.y = y_;
+		p.z = z_;
+		p.label = label_;
+		return p;
+	}
 
 	// Creates a point with XYZ coordinates and a RGB color definition associated
-	static pcl::PointXYZRGB createPointXYZRGB(const float _x, const float _y, const float _z, const uint8_t _r, const uint8_t _g, const uint8_t _b);
+	static inline pcl::PointXYZRGB createPointXYZRGB(const float _x, const float _y, const float _z, const uint8_t _r, const uint8_t _g, const uint8_t _b)
+	{
+		pcl::PointXYZRGB p;
+		p.x = _x;
+		p.y = _y;
+		p.z = _z;
+		p.rgba = Utils::getColor(_r, _g, _b);
+		return p;
+	}
 
 	// Creates a point with XYZ coordinates and a color definition associated
-	static pcl::PointXYZRGB createPointXYZRGB(const float _x, const float _y, const float _z, const PointColor &_color);
+	static inline pcl::PointXYZRGB createPointXYZRGB(const float _x, const float _y, const float _z, const PointColor &_color)
+	{
+		pcl::PointXYZRGB p;
+		p.x = _x;
+		p.y = _y;
+		p.z = _z;
+		p.rgba = _color;
+		return p;
+	}
 
 	// Creates a point with XYZ coordinates, a RGB color definition and a normal vector associated to each point
-	static pcl::PointXYZRGBNormal createPointXYZRGBNormal(const float _x, const float _y, const float _z, const float _nx, const float _ny, const float _nz, const float _curvature, const uint8_t _r, const uint8_t _g, const uint8_t _b);
+	static inline pcl::PointXYZRGBNormal createPointXYZRGBNormal(const float _x, const float _y, const float _z, const float _nx, const float _ny, const float _nz, const float _curvature, const uint8_t _r, const uint8_t _g, const uint8_t _b)
+	{
+		pcl::PointXYZRGBNormal p;
+		p.x = _x;
+		p.y = _y;
+		p.z = _z;
+		p.normal_x = _nx;
+		p.normal_y = _ny;
+		p.normal_z = _nz;
+		p.curvature = _curvature;
+		p.rgba = Utils::getColor(_r, _g, _b);
+		return p;
+	}
 
 	// Creates a point with XYZ coordinates, a RGB color definition and a normal vector associated to each point
-	static pcl::PointXYZRGBNormal createPointXYZRGBNormal(const float _x, const float _y, const float _z, const float _nx, const float _ny, const float _nz, const float _curvature, const PointColor &_color);
+	static inline pcl::PointXYZRGBNormal createPointXYZRGBNormal(const float _x, const float _y, const float _z, const float _nx, const float _ny, const float _nz, const float _curvature, const PointColor &_color)
+	{
+		pcl::PointXYZRGBNormal p;
+		p.x = _x;
+		p.y = _y;
+		p.z = _z;
+		p.normal_x = _nx;
+		p.normal_y = _ny;
+		p.normal_z = _nz;
+		p.curvature = _curvature;
+		p.rgba = _color;
+		return p;
+	}
 
 	// Creates a point with XYZ coordinates, a RGB color definition and a normal vector associated to each point
-	static pcl::PointXYZRGBNormal createPointXYZRGBNormal(const float _x, const float _y, const float _z, const float _nx, const float _ny, const float _nz, const float _curvature, const uint32_t &_color);
+	static inline pcl::PointXYZRGBNormal createPointXYZRGBNormal(const float _x, const float _y, const float _z, const float _nx, const float _ny, const float _nz, const float _curvature, const uint32_t &_color)
+	{
+		pcl::PointXYZRGBNormal p;
+		p.x = _x;
+		p.y = _y;
+		p.z = _z;
+		p.normal_x = _nx;
+		p.normal_y = _ny;
+		p.normal_z = _nz;
+		p.curvature = _curvature;
+		p.rgba = _color;
+		return p;
+	}
 
 	// Creates a point with XYZ coordinates and a normal vector associated to each point
-	static pcl::PointNormal createPointNormal(const float _x, const float _y, const float _z, const float _nx, const float _ny, const float _nz, const float _curvature = 0);
+	static inline pcl::PointNormal createPointNormal(const float x_, const float y_, const float z_, const float nx_, const float ny_, const float nz_, const float curvature_ = 0)
+	{
+		pcl::PointNormal p;
+		p.x = x_;
+		p.y = y_;
+		p.z = z_;
+		p.normal_x = nx_;
+		p.normal_y = ny_;
+		p.normal_z = nz_;
+		p.curvature = curvature_;
+		return p;
+	}
 
 private:
 	PointFactory();
